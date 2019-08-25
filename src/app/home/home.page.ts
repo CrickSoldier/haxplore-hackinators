964e6b2067f82d6b6d41f90e22c6b7a30b168db1
shubhamkarala <shubhamkarala@gmail.com>
2019-08-25 11:42:34 +0530

    initial Commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { LoadingController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
+import { GoogleMaps, GoogleMap } from '@ionic-native/google-maps';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  @ViewChild('map') mapElement: ElementRef;
+  map: GoogleMap;
+
+  items: Array<any>;
+
+  constructor(
+    public loadingCtrl: LoadingController,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private _googleMaps: GoogleMaps
+  ) { }
+  ngAfterViewInit() {
+    this.initMap();
+  }
+
+  initMap() {
+    let element = this.mapElement.nativeElement;
+    this.map = this._googleMaps.create(element)
+  }
+
+  ngOnInit() {
+    if (this.route && this.route.data) {
+      this.getData();
+    }
+  }
+
+  async getData(){
+    const loading = await this.loadingCtrl.create({
+      message: 'Please wait...'
+    });
+    this.presentLoading(loading);
+
+    this.route.data.subscribe(routeData => {
+      routeData['data'].subscribe(data => {
+        loading.dismiss();
+        this.items = data;
+      })
+    })
+  }
+
+  async presentLoading(loading) {
+    return await loading.present();
+  }
+
+  logout(){
+    this.authService.doLogout()
+    .then(res => {
+      this.router.navigate(["/login"]);
+    }, err => {
+      console.log(err);
+    })
+  }
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,38 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+// export const environment = {
+//   production: false,
+//   firebase : {
+//     apiKey: "YOUR_CREDENTIALS_HERE",
+//     authDomain: "YOUR_CREDENTIALS_HERE",
+//     databaseURL: "YOUR_CREDENTIALS_HERE",
+//     projectId: "YOUR_CREDENTIALS_HERE",
+//     storageBucket: "YOUR_CREDENTIALS_HERE",
+//     messagingSenderId: "YOUR_CREDENTIALS_HERE"
+//   }
+// };
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyBvfKYoDpDPxo5XgOen0l_sBm5i5vjthr0",
+    authDomain: "medieagle-f3484.firebaseapp.com",
+    databaseURL: "https://medieagle-f3484.firebaseio.com",
+    projectId: "medieagle-f3484",
+    storageBucket: "medieagle-f3484.appspot.com",
+    messagingSenderId: "243733219113",
+    appId: "1:243733219113:web:8c44a532ec5cf4be"
+  }
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
